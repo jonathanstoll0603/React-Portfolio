076fef6c56ae48496bf80e71abc247f505e94d43
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-05-07 23:07:25 -0400

    added route handling for pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Projects from "./pages/Projects";
-import AboutMe from "./pages/AboutMe";
+import About from "./pages/About";
 import Resume from "./pages/Resume";
+import Quotes from "./pages/Quotes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -13,11 +14,11 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={AboutMe} />
-          <Route exact path="/about" component={AboutMe} />
-          {/* <Route exact path="/projects" component={Projects} />
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
           <Route exact path="/quotes" component={Quotes} />
-          <Route exact path="/resume" component={Resume} /> */}
+          <Route exact path="/resume" component={Resume} />
         </Wrapper>
         <Footer />
       </div>
