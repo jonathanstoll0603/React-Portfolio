386fb44d7f798c533b22163c1dad6d2a58a286cf
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-05-07 23:08:02 -0400

    completed initial styling and information for About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,105 +1,32 @@
 import React, { Component } from "react";
-import Container from "../components/Container/Container";
-import Table from "../components/Table/Table";
-import API from "../utils/API";
-import Search from "../components/Search/Search";
+import "./About.css";
+import Container from "../components/Container";
+import profilePic from "../assets/images/jon-in-cali.jpg";
 
 class Home extends Component {
-  state = {
-    results: [],
-    currentSort: "desc",
-    search: "",
-    error: ""
-  };
-
-  componentDidMount() {
-    API.getEmployees()
-      .then(res => {
-        this.setState({ results: res.data.results });
-      })
-      .catch(err => console.log(err));
-  }
-
-  // function that handles filtering of search results upon typed values
-  handleNameSearch = event => {
-    event.preventDefault();
-
-    const currentSearch = event.target.value;
-
-    const filteredState = this.state.results.filter((result) => {
-      let values = result.name.first.toLowerCase() + " " + result.name.last.toLowerCase();
-
-      if (values.indexOf(currentSearch) !== -1) {
-        return result;
-      }
-    })
-
-    this.setState({ results: filteredState });
-
-  };
-  
-  // function that handles ordering names by ascending or descending order on button click
-  handleNameSort = () => {
-    const nameSortAsc = [].concat(this.state.results)
-    .sort((a, b) => a.name.first > b.name.first ? 1 : -1);
-    const nameSortDesc = [].concat(this.state.results)
-    .sort((a, b) => b.name.first > a.name.first ? 1 : -1);
-
-    switch(this.state.currentSort) {
-      case "desc":
-        this.setState({ results: nameSortAsc, currentSort: "asc" });
-        break;
-      case "asc":
-        this.setState({ results: nameSortDesc, currentSort: "desc" });
-        break;
-    }
-  }
-
-  // function that handles ordering emails by ascending or descending order on button click
-  handleEmailSort = () => {
-    const emailSortAsc = [].concat(this.state.results)
-    .sort((a, b) => a.email > b.email ? 1 : -1);
-    const emailSortDesc = [].concat(this.state.results)
-    .sort((a, b) => b.email > a.email ? 1 : -1);
-
-    switch(this.state.currentSort) {
-      case "desc":
-        this.setState({ results: emailSortAsc, currentSort: "asc" });
-        break;
-      case "asc":
-        this.setState({ results: emailSortDesc, currentSort: "desc" });
-        break;
-    }
-  }
-
-  // function that handles ordering birthdays by ascending or descending order on button click
-  handleDOBSort = () => {
-    const dobSortAsc = [].concat(this.state.results)
-    .sort((a, b) => parseInt(a.dob.date) - parseInt(b.dob.date));
-    const dobSortDesc = [].concat(this.state.results)
-    .sort((a, b) => parseInt(b.dob.date) - parseInt(a.dob.date) );
-
-    switch(this.state.currentSort) {
-      case "desc":
-        this.setState({ results: dobSortAsc, currentSort: "asc" });
-        break;
-      case "asc":
-        this.setState({ results: dobSortDesc, currentSort: "desc" });
-        break;
-    }   
-  }
 
   render() {
     return (
         <Container>
-            <Search handleNameSearch={this.handleNameSearch} />
-            <Table 
-              results={this.state.results}
-              handleNameSort={this.handleNameSort}
-              handleDOBSort={this.handleDOBSort}
-              handleEmailSort={this.handleEmailSort}
-            >
-            </Table>
+          <section>
+            <h1 className="my-3 text-center">About me</h1>
+          </section>
+          <section className="row row-cols-1 mx-4 text-center">
+              <div className="col-md-12">
+                <img src={profilePic} className="profile-pic" alt="Jon Stoll in La Jolla, California on the coastline"></img>
+                <p className="py-3">
+                  Welcome to my React Portfolio! To give some background on my life, my name is Jonathan Stoll and I am a 26 year old LIMS Engineer Atlanta, Georgia. I enjoy traveling, live music, meditation, breweries, politics, pups, EPL soccer, basketball, and everything outdoors. Although I've been practicing web development on my own for the better part of a year, I recently decided to dive into the immersive full stack web development program offered through Georgia Tech. I'm hoping to gain the skills needed to further my career and pursue my passions. 
+                </p>
+              </div>
+            </section>
+            <section className="row row-cols-1 mx-4 text-center">
+              <div className="col-md-12">
+                <p className="py-3">
+                  One passion in particular that I have come to love is healthcare LIS/LIMS systems. This is in part due to the fact that I have been given the opportunity to work as my current department's LIMS Analyst where I have gained extensive knowledge building, validating, and improving the organization's data management software. In recent decades, these systems have predominantly moved to browser based applications that offer powerful capabilities not seen in past systems. Unfortunately, many of these browser based programs run on outdated languages that offer dismal wait times and clunky UI's. My goal is to graduate from the full stack web development program with the knowledge needed to improve and even recreate these systems from scratch. With the development of newer technologies and integration of up-to-date languages, millions more patients could benefit, and healthcare organizations can save enormous amounts of time and money.
+                </p>
+              </div>
+            </section>
+
         </Container>
     );
   }
