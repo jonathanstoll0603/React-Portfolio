d0adbe8251e8f05fbadba94bc6d85e7be06caef6
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-05-11 01:50:09 -0400

    finished styling of pages

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,24 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand mx-3" to="/">
+    <nav className="navbar navbar-expand-lg navbar-light navbar-container">
+      <Link className="navbar-brand mx-3 text-light" to="/">
         Jon's Portfolio
       </Link>
       <div>
         <ul className="navbar-nav">
-          <li className="nav-item">
+          <li className="nav-item text-light">
             <Link
               to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
+              className={window.location.pathname === "/" || window.location.pathname === "/about" ? "nav-link active text-light": "nav-link text-light"}
             >
               About Me
             </Link>
@@ -27,7 +22,7 @@ function Navbar() {
           <li className="nav-item">
             <Link
               to="/projects"
-              className={window.location.pathname === "/projects" ? "nav-link active" : "nav-link"}
+              className={window.location.pathname === "/projects" ? "nav-link active text-light" : "nav-link text-light"}
             >
               Projects
             </Link>
@@ -35,19 +30,19 @@ function Navbar() {
           <li className="nav-item">
             <Link
               to="/resume"
-              className={window.location.pathname === "/resume" ? "nav-link active" : "nav-link"}
+              className={window.location.pathname === "/resume" ? "nav-link active text-light" : "nav-link text-light"}
             >
               Resume
             </Link>
           </li>
-          <li className="nav-item">
+          {/* <li className="nav-item">
             <Link
               to="/quotes"
-              className={window.location.pathname === "/quotes" ? "nav-link active" : "nav-link"}
+              className={window.location.pathname === "/quotes" ? "nav-link active text-light" : "nav-link text-light"}
             >
               Quotes
             </Link>
-          </li>
+          </li> */}
         </ul>
       </div>
     </nav>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
 import "./About.css";
 import Container from "../components/Container";
-import profilePic from "../assets/images/jon-in-cali.jpg";
+import profilePic from "../assets/images/jon-atl-skyline.JPG";
 
 class Home extends Component {
 
   render() {
     return (
         <Container>
-          <section>
-            <h1 className="my-3 text-center text-info">About me</h1>
-          </section>
-          <section className="row row-cols-1 mx-4 text-center text-info">
-              <div className="col-md-12">
-                <img src={profilePic} className="profile-pic" alt="Jon Stoll in La Jolla, California on the coastline"></img>
+          {/* <section>
+            <h1 className="text-center py-3 about-header">About me</h1>
+          </section> */}
+          <section className="row row-cols-2 mx-4 py-5 text-center text-white">
+              <div className="col-md-3 my-3">
+                <img src={profilePic} className="profile-pic" alt="Jon Stoll in Atlanta Georgia with the City skyline behind him."></img>
+              </div>
+              <div className="col-md-9 my-3 paragraph-div">
                 <p className="py-3">
                   Welcome to my React Portfolio! To give some background on my life, my name is Jonathan Stoll and I am a 26 year old LIMS Engineer Atlanta, Georgia. I enjoy traveling, live music, meditation, breweries, politics, pups, EPL soccer, basketball, and everything outdoors. Although I've been practicing web development on my own for the better part of a year, I recently decided to dive into the immersive full stack web development program offered through Georgia Tech. I'm hoping to gain the skills needed to further my career and pursue my passions. 
                 </p>
               </div>
             </section>
-            <section className="row row-cols-1 mx-4 text-center text-info">
+            <section className="row row-cols-1 mx-4 text-center text-white">
               <div className="col-md-12">
                 <p className="py-3">
                   One passion in particular that I have come to love is healthcare LIS/LIMS systems. This is in part due to the fact that I have been given the opportunity to work as my current department's LIMS Analyst where I have gained extensive knowledge building, validating, and improving the organization's data management software. In recent decades, these systems have predominantly moved to browser based applications that offer powerful capabilities not seen in past systems. Unfortunately, many of these browser based programs run on outdated languages that offer dismal wait times and clunky UI's. My goal is to graduate from the full stack web development program with the knowledge needed to improve and even recreate these systems from scratch. With the development of newer technologies and integration of up-to-date languages, millions more patients could benefit, and healthcare organizations can save enormous amounts of time and money.
